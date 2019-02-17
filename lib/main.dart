1b4c8af21e3bef36c23ca4867366f8664147cfee
akkk33 <akkk33@protonmail.com>
2019-02-17 07:58:30 +0200

    + Project files

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tayar/user/main.dart';
+// User's pages:
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Layout direction:
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale("ar", "EG"),
+      ],
+      locale: Locale("ar", "EG"),
+      // General app settings
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.orange, fontFamily: 'Frutiger'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentPage = 0;
+  final List<Widget> _pages = [MainList(), MainList(), MainList()];
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(),
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: updateCurrentPage,
+        currentIndex: _currentPage,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              activeIcon: Icon(
+                Icons.home,
+                color: Theme.of(context).accentColor,
+              ),
+              title: Text("الرئيسية")),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text("سلة المشتريات"),
+            activeIcon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              title: Text("الإعدادات"),
+              activeIcon: Icon(
+                Icons.account_circle,
+                color: Theme.of(context).accentColor,
+              )),
+        ],
+      ),
+    );
+  }
+
+  void updateCurrentPage(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+}
