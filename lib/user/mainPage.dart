618a860891e4d0588b79aa9035b020705794524d
akkk33 <akkk33@protonmail.com>
2019-02-18 06:50:54 +0200

    * Section grid elements alignment

--- lib/user/mainPage.dart
+++ lib/user/mainPage.dart
@@ -5,6 +5,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.fromLTRB(5.0, 15.0, 5.0, 5.0),
       child: _MainSectionsWidget(),
     );
   }
@@ -22,12 +23,14 @@ class _MainSections extends State<_MainSectionsWidget> {
       crossAxisCount: 2,
       children: List.generate(16, (index) {
         return FlatButton(
+          padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
           child: Stack(
+            alignment: AlignmentDirectional(0, 0),
             children: <Widget>[
               CachedNetworkImage(
                 placeholder: CircularProgressIndicator(),
                 imageUrl:
-                    "http://www.clker.com/cliparts/3/3/K/d/7/k/purple-square-light-hi.png",
+                "http://www.clker.com/cliparts/3/3/K/d/7/k/purple-square-light-hi.png",
               ),
               Center(
                 child: Text("section"),
