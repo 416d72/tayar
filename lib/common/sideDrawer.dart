078bdacb126714181222828eff82154d9412820d
akkk33 <akkk33@protonmail.com>
2019-02-19 02:43:41 +0200

    * Split side-drawer items into two parts

--- lib/common/sideDrawer.dart
+++ lib/common/sideDrawer.dart
@@ -6,7 +6,7 @@ class SideDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Column(
         children: <Widget>[
           DrawerHeader(
             child: Column(
@@ -20,35 +20,48 @@ class SideDrawer extends StatelessWidget {
               ],
             ),
           ),
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text("المنتجات المفضلة"),
-            onTap: null,
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text("المنتجات المفضلة"),
+                  onTap: null,
+                ),
+                Divider(
+                  height: 0.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.store),
+                  title: Text("المتاجر"),
+                  onTap: null,
+                ),
+                Divider(
+                  height: 0.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("الإعدادات"),
+                  onTap: null,
+                ),
+              ],
+            ),
           ),
-          Divider(
-            height: 0.0,
-          ),
-          ListTile(
-            leading: Icon(Icons.store),
-            title: Text("المتاجر"),
-            onTap: null,
-          ),
-          Divider(
-            height: 0.0,
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text("الإعدادات"),
-            onTap: null,
-          ),
-          Divider(
-            height: 0.0,
-          ),
-          ListTile(
-            leading: Icon(Icons.report_problem),
-            title: Text("بلغ عن مشكلة"),
-            onTap: null,
-          ),
+          Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: Column(
+              children: <Widget>[
+                Divider(
+                  height: 0.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.report_problem),
+                  title: Text("بلغ عن مشكلة"),
+                  onTap: null,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
--- lib/generated/i18n.dart
+++ lib/generated/i18n.dart
@@ -18,6 +18,7 @@ class S implements WidgetsLocalizations {
 
   @override
   TextDirection get textDirection => TextDirection.ltr;
+
 }
 
 class $en extends S {
@@ -108,9 +109,8 @@ class GeneratedLocalizationsDelegate extends LocalizationsDelegate<S> {
         }
 
         // If no country requirement is requested, check if this locale has no country.
-        if (true != withCountry &&
-            (supportedLocale.countryCode == null ||
-                supportedLocale.countryCode.isEmpty)) {
+        if (true != withCountry && (supportedLocale.countryCode == null ||
+            supportedLocale.countryCode.isEmpty)) {
           return true;
         }
       }
