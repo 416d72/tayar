549c253ea4452bcba4beeb561697a8fc4fe8f6d5
akkk33 <akkk33@protonmail.com>
2019-02-21 04:41:56 +0200

    * What a mess!!! two days trying to refactor in the complete wrong  class 😬😬😬

--- lib/users/client/mainPage.dart
+++ lib/users/client/mainPage.dart
@@ -10,7 +10,7 @@ class _MainSections extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return SectionsPage(
-      parent: '/',
+      collection: 'Sections',
     );
   }
 }
