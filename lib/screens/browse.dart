2a298e37f20bd5b9d3b0aaa49f079fc0e39cce42
akkk33 <akkk33@protonmail.com>
2019-02-23 04:49:58 +0200

    * Separated browser into (default view - fancy view)

--- lib/screens/browse.dart
+++ lib/screens/browse.dart
@@ -1,13 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tayar/app.dart';
 import 'package:tayar/widgets/scaffold.dart';
 
 class BrowsePage extends StatefulWidget {
   final String collection;
   final String parent;
+  final bool fancy;
 
-  const BrowsePage({Key key, @required this.collection, @required this.parent})
+  const BrowsePage(
+      {Key key, @required this.collection, @required this.parent, this.fancy})
       : super(key: key);
   @override
   State<StatefulWidget> createState() {
@@ -18,6 +21,18 @@ class BrowsePage extends StatefulWidget {
 class _BrowsePageState extends State<BrowsePage> {
   @override
   Widget build(BuildContext context) {
+    if (widget.fancy) {
+      return CustomScaffold(
+        body: Center(
+          child: Text('Fancy'),
+        ),
+      );
+    } else {
+      return _defaultGrid();
+    }
+  }
+
+  Widget _defaultGrid() {
     return CustomScaffold(
       body: StreamBuilder(
         stream: Firestore.instance
@@ -34,8 +49,14 @@ class _BrowsePageState extends State<BrowsePage> {
             crossAxisCount: 2,
             children: List.generate(snapshot.data.documents.length, (index) {
               var section = snapshot.data.documents[index];
-              return sectionCard(context, section.reference.documentID,
-                  section['title'], section['image'], section['child']);
+              var id = section.reference.documentID;
+              if (section['child'] == 'Sections') {
+                return sectionCard(context, id, section['title'],
+                    section['image'], section['child']);
+              } else {
+                return productCard(context, id, section['title'],
+                    section['image'], section['child']);
+              }
             }),
           );
         },
@@ -43,30 +64,12 @@ class _BrowsePageState extends State<BrowsePage> {
     );
   }
 
-  Widget sectionCard(BuildContext context, String parent, String title,
+  Widget sectionCard(BuildContext context, String documentID, String title,
       String image, String child) {
     return GestureDetector(
-//      onTap: () {
-//        setState(() {
-//          _stream = Firestore.instance
-//              .collection(child)
-//              .where('active', isEqualTo: true)
-//              .where('parent', isEqualTo: '$parent')
-//              .snapshots();
-//        });
-//      },
       onTap: () {
-        return showDialog(
-            context: context,
-            child: AlertDialog(
-              content: Text("Products of $title"),
-              actions: <Widget>[
-                FlatButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: Text('Close'),
-                )
-              ],
-            ));
+        return App.router.navigateTo(
+            context, '/browse?collection=$child&parent=$documentID');
       },
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
@@ -105,6 +108,64 @@ class _BrowsePageState extends State<BrowsePage> {
                   child: Text(
                     title,
                     style: Theme.of(context).textTheme.subhead,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget productCard(BuildContext context, String documentID, String title,
+      String image, String child) {
+    return GestureDetector(
+      onTap: () {
+        print(documentID);
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Theme
+                        .of(context)
+                        .accentColor,
+                    blurRadius: 1.0,
+                    spreadRadius: 0.0,
+                  ),
+                ],
+              ),
+              child: CachedNetworkImage(
+                imageUrl: image,
+                placeholder: Center(
+                  child: CircularProgressIndicator(),
+                ),
+                errorWidget: Text("Error loading image"),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  height: 30.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Text(
+                    title,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .subhead,
                   ),
                 ),
               ],
